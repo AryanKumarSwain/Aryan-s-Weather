828818632af781421d6b48d4993f50930814069c
Aryan <aryannn.ks@gmail.com>
2024-09-23 22:37:25 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,18 +7,18 @@ import { GoUnmute } from "react-icons/go";
 
 const Navbar = ({ toggleMute, isMuted }) => {
     useEffect(() => {
-        const logoDark = document.querySelector('.logo-dark');
+        const logoElement = document.querySelector('.logo-dark');
         const socialIcons = document.querySelectorAll('.social-icon');
 
         const handleScroll = () => {
             if (window.scrollY > 50) {
-                logoDark?.classList.add('glow-dark');
+                logoElement?.classList.add('glow-dark');
                 
                 socialIcons.forEach(icon => {
                     icon.classList.add('icon-glow-dark');
                 });
             } else {
-                logoDark?.classList.remove('glow-dark');
+                logoElement?.classList.remove('glow-dark');
                 
                 socialIcons.forEach(icon => {
                     icon.classList.remove('icon-glow-dark');
@@ -37,28 +37,28 @@ const Navbar = ({ toggleMute, isMuted }) => {
         <nav className="fixed mt-2 w-full px-6 flex items-center justify-between z-50">
             <div className="flex items-center justify-center">
                 <a href="https://swain.netlify.app/" target="_blank" rel="noopener noreferrer">
-                    <img className="logo-dark hover:contrast-180 w-36 cursor-pointer rounded-xl  p-2" src={logo} alt="logo" />
+                    <img className="logo-dark hover:contrast-180 w-36 cursor-pointer rounded-xl p-2" src={logo} alt="logo" />
                 </a>
             </div>
             
             <div className="hidden md:flex ml-auto m-6 items-center justify-center gap-4 text-2xl text-white">
                 <ul className="flex m-6 items-center justify-center gap-4 text-2xl">
-                    <li className="hover:contrast-150 social-icon rounded-full  p-1">
+                    <li className="hover:contrast-150 social-icon rounded-full p-1">
                         <a href="https://linkedin.com/in/aryannn-ks/" target="_blank" rel="noopener noreferrer">
                             <FaLinkedin />
                         </a>
                     </li>
-                    <li className="hover:contrast-150 social-icon rounded-full  p-1">
+                    <li className="hover:contrast-150 social-icon rounded-full p-1">
                         <a href="https://github.com/AryanKumarSwain" target="_blank" rel="noopener noreferrer">
                             <FaGithub />
                         </a>
                     </li>
-                    <li className="hover:contrast-150 social-icon  rounded-full  p-1">
+                    <li className="hover:contrast-150 social-icon rounded-full p-1">
                         <a href="https://instagram.com/AryanKumarSwain" target="_blank" rel="noopener noreferrer">
                             <FaInstagram />
                         </a>
                     </li>
-                    <li className="hover:contrast-150 social-icon  rounded-full  p-1">
+                    <li className="hover:contrast-150 social-icon rounded-full p-1">
                         <a href="https://x.com/Aryannn_KS" target="_blank" rel="noopener noreferrer">
                             <FaXTwitter />
                         </a>
@@ -68,7 +68,7 @@ const Navbar = ({ toggleMute, isMuted }) => {
             
             <button
                 onClick={toggleMute}
-                className="text-2xl hover:contrast-150 text-white social-icon rounded-full  p-1"
+                className="text-2xl hover:contrast-150 text-white social-icon rounded-full p-1"
             >
                 {isMuted ? <VscMute /> : <GoUnmute />}
             </button>
